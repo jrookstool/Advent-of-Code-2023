9f0f5b33ccdd0c585a00185bd94fe3fa09f5be44
Jack Rookstool <jrooksto@purdue.edu>
2023-12-06 00:45:48 -0500

    day 2 done -- super easy

--- day2.c
+++ day2.c
@@ -6,6 +6,8 @@
 #define MAX_LENGTH 256
 
 int validateLine(char *line, int counts[]);
+int validateClause(char *clause, int counts[]);
+int findMinColor(char *line);
 
 int verifyGames(char *filename, int counts[]) {
     FILE *file = fopen(filename, "r");
@@ -33,6 +35,26 @@ int verifyGames(char *filename, int counts[]) {
     return sum;
 }
 
+int findTotalPower(char *filename, int counts[]) {
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL) {
+        perror("fopen\n");
+        exit(1);
+    }
+
+    char line[MAX_LENGTH];
+    int power = 0;
+
+    while (fgets(line, 256, file) != NULL) {
+        power += findMinColor(line);
+    }
+
+    fclose(file);
+
+    return power;
+}
+
 int validateLine(char *line, int counts[]) {
     line = strchr(line, ':');
     line++;
@@ -54,9 +76,54 @@ int validateLine(char *line, int counts[]) {
     return 1;
 }
 
+int findMinColor(char *line) {
+    line = strchr(line, ':');
+    line++;
+
+    while (*line == ' ') line++;
+
+    int maxRed = 0;
+    int maxGreen = 0;
+    int maxBlue = 0;
+
+    char *saveptr;
+    char *tuples = strtok_s(line, ";", &saveptr);
+    assert(tuples != NULL);
+    while (tuples != NULL) {
+        char *saveptr2;
+        char *values = strtok_s(tuples, ",", &saveptr2);
+        assert(values != NULL);
+        while (values != NULL) {
+            int number = 0;
+            char color[6];
+            sscanf(values, "%d %s", &number, color);
+            if (strcmp(color, "red") == 0) {
+                if (maxRed == 0 || number > maxRed) {
+                    maxRed = number;
+                }
+            } else if (strcmp(color, "green") == 0) {
+                if (maxGreen == 0 || number > maxGreen) {
+                    maxGreen = number;
+                }
+            } else if (strcmp(color, "blue") == 0){
+                if (maxBlue == 0 || number > maxBlue) {
+                    maxBlue = number;
+                }
+            }
+            values = strtok_s(NULL, ",", &saveptr2);
+        }
+        tuples = strtok_s(NULL, ";", &saveptr);
+    }
+
+    printf("line is: %s\n", line);
+    printf("minRed: %d\nminGreen: %d\nminBlue: %d\n", maxRed, maxGreen, maxBlue);
+
+    return maxRed * maxGreen * maxBlue;
+}
+
 int validateClause(char *clause, int counts[]) {
     char *saveptr;
-    char *values = strtok_r(clause, ",", &saveptr);
+    char *values = strtok_s(clause, ",", &saveptr);
     while (values != NULL) {
         int number = 0;
         char color[6];
@@ -76,7 +143,7 @@ int validateClause(char *clause, int counts[]) {
             }
         }
 
-        values = strtok_r(NULL, ",", &saveptr);
+        values = strtok_s(NULL, ",", &saveptr);
     }
     
     return 1;
@@ -85,6 +152,7 @@ int validateClause(char *clause, int counts[]) {
 int main() {
     int counts[3] = {12, 13, 14};
     printf("%d\n", verifyGames("day2_input.txt", counts));
+    printf("Total power: %d\n", findTotalPower("day2_input.txt", counts));
     return 0;
 }
 
