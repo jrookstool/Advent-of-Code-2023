3fdc5d5c0dc7fbdd4f08a4851627429b976ae3c1
Jack Rookstool <jrooksto@purdue.edu>
2023-12-06 10:09:43 -0500

    core logic down for day 3!

--- Day 3 (C++)/day3.cpp
+++ Day 3 (C++)/day3.cpp
@@ -36,31 +36,106 @@ int findSum(string beforeLine, string currentLine, string afterLine) {
     int startIndex = 0;
     int endIndex = currentLine.length();
 
-    vector<array<int, 2>> numbers;
+    int sum = 0;
+
+    vector<array<int, 2>> indices;
+    vector<int> numbers;
     bool finished = false;
 
     for (int i = 0; i < currentLine.length(); i++) {
-        if (!isSpecialCharacter(currentLine[i])) {
+        if (isdigit(currentLine[i])) {
             startIndex = i;
             if (!finished) {
                 for (int j = i + 1; j < currentLine.length(); j++) {
-                    if (!isSpecialCharacter(currentLine[j])) {
+                    if (!isdigit(currentLine[j])) {
                         endIndex = j;
-                        numbers.push_back({ startIndex, endIndex });
+                        indices.push_back({ startIndex, endIndex });
+                        numbers.push_back(stoi(currentLine.substr(startIndex, endIndex - startIndex)));
                         finished = true;
+                        i = j;
+                        break;
                     }
                 }
             }
-            
+            finished = false;
         }
     }
 
     for (const auto& number : numbers) {
-        cout << "[" << number[0] << ", " << number[1] << "] ";
+        cout << "[" << number << "] ";
     }
     cout << endl;
 
-    return 0;
+    // Now that we have all numbers, we need to find the ones that are adjacent to special characters.
+
+    for (int i = 0; i < numbers.size(); i++) {
+        if (isSpecialCharacter(currentLine[indices[i][0] - 1])) {
+            cout << "Found a special character to the left of " << numbers[i] << endl;
+            sum += numbers[i];
+            continue;
+        }
+        if (isSpecialCharacter(currentLine[indices[i][1]])) {
+            cout << "Found a special character to the right of " << numbers[i] << endl;
+            sum += numbers[i];
+            continue;
+        }
+
+        if (beforeLine != "") {
+            for (int j = indices[i][0]; j < indices[i][1]; j++) {
+                if (isSpecialCharacter(beforeLine[j])) {
+                    cout << "Found a special character above " << numbers[i] << endl;
+                    sum += numbers[i];
+                    break;
+                }
+            }
+            continue;
+
+            if (isSpecialCharacter(beforeLine[indices[i][0] - 1])) {
+                cout << "Found a special character top left of " << numbers[i] << endl;
+                sum += numbers[i];
+                continue;
+            }
+
+            if (isSpecialCharacter(beforeLine[indices[i][1]])) {
+                cout << "Found a special character top right of " << numbers[i] << endl;
+                sum += numbers[i];
+                continue;
+            }
+        }
+        if (afterLine != "") {
+            for (int j = indices[i][0]; j < indices[i][1]; j++) {
+                if (isSpecialCharacter(afterLine[j])) {
+                    cout << "Found a special character below " << numbers[i] << endl;
+                    sum += numbers[i];
+                }
+                break;
+            }
+            continue;
+
+            if (isSpecialCharacter(afterLine[indices[i][0] - 1])) {
+                cout << "Found a special character bottom left of " << numbers[i] << endl;
+                sum += numbers[i];
+                continue;
+            }
+
+            if (isSpecialCharacter(afterLine[indices[i][1]])) {
+                cout << "Found a special character bottom right of " << numbers[i] << endl;
+                sum += numbers[i];
+                continue;
+            }
+            if (isSpecialCharacter(beforeLine[indices[i][0]])) {
+                cout << "Found a special character above " << numbers[i] << endl;
+                sum += numbers[i];
+                continue;
+            }
+            if (isSpecialCharacter(beforeLine[indices[i][1]])) {
+                cout << "Found a special character above " << numbers[i] << endl;
+                sum += numbers[i];
+            }
+        }
+    }
+
+    return sum;
 }
 
 
@@ -68,7 +143,8 @@ int main() {
     string line;
     ifstream myfile("day3_input.txt");
 
-    cout << isSpecialCharacter('4') << endl;
-
-    cout << findSum("", ".....487.599...........411...........................................574..679.136..........................30......255.......432............", "") << endl;
+    //cout << findSum("", ".....487.599...........411...........................................574..679.136..........................30......255.......432............", "") << endl;
+    cout << findSum("....*......*............*..........&586..........................375...@..*....../.....835.............610*........./...............582.....",
+                    "...833........304...&.862...............203..........922.125...............819.............@....563.....................722..775............",
+                    "..............+...994..........#.........*..244.457.....*...........867.........829.....469.....#...........................*...............") << endl;
 }
