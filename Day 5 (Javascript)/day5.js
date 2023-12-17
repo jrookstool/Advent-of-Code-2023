3a5a14ef393dffac305c3d9847b3421255483ab3
Jack Rookstool <jrooksto@purdue.edu>
2023-12-16 21:34:29 -0500

    very surface level file parsing

--- Day 5 (Javascript)/day5.js
+++ Day 5 (Javascript)/day5.js
@@ -9,8 +9,17 @@ function handleFileInput(event) {
 
         reader.onload = function (e) {
             const data = e.target.result;
-            console.log(data);
-            console.log("test");
+            const lines = data.split('\n');
+            const seeds = lines[0].split(": ")[1];
+            const seedsArray = seeds.split(" ");
+            for (let i = 0; i < seedsArray.length; i++) {
+                seedsArray[i] = parseInt(seedsArray[i]);
+            }
+            for (let i = 0; i < seedsArray.length; i++) {
+                if (seedsArray[i] < 1263068588) {
+                    console.log(seedsArray[i]);
+                } 
+            }
         };
 
         reader.readAsText(file);
